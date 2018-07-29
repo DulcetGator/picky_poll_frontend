1255ae872d7ef47dd02f6719139f20993f56a6e8
Stephen Mercado <stephenmerc@gmail.com>
2018-07-28 21:37:24 -0700

    Add Flow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
+type Props = {};
+
+class App extends Component<Props> {
   render() {
     return (
       <div className="App">
