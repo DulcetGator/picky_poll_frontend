d2341b33594850687036904b2fd482c8356057b0
Stephen Mercado <stephenmerc@gmail.com>
2021-02-23 02:25:27 +0000

    Fix setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ const proxy = require('http-proxy-middleware');
 
 const isProd = process.env.BACKEND_ENV === 'prod';
 const backend = isProd
-  ? 'https://pickypoll.com/api/'
-  : 'http://localhost:8080/';
+  ? 'https://pickypoll.com'
+  : 'http://localhost:8080';
 
 const pathRewriteTarget = isProd
   ? '/api/'
